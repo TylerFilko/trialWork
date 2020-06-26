84879a877cec4af4c44ccb2f7b6c3241504801fb
TylerFilko <tylerfilko@gmail.com>
2020-06-26 15:50:02 -0500

    updates

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-pipeline-source-bucket"
+    key    = "tf-state/"
+    region = "us-east-1"
+    dynamodb_table = "tf-pipeline-terraform-lock"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test-tf-pipeline-bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "delete_me"
+    Environment = "delete_me"
+  }
+}
